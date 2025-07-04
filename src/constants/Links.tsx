ed6ecf7412b0c77da4d52bfe22b82b675a648741
DavidGarzonDev <juanxavif@gmail.com>
2025-07-04 16:40:25 -0500

    añadiendo los links iniciales de la pagina

---
+++ src/constants/Links.tsx
@@ -0,0 +1,16 @@
+export const navbarLinks = [
+    {   id: 1,
+        title: "Inicio",
+        href: "/"
+    },
+    {
+        id: 2,
+        title: "Hombre",
+        href: "/man"
+    },
+    {
+        id: 3,
+        title: "Mujer",
+        href: "/woman"
+    }
+]
