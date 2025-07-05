b0a46438a8717913b800c37d7209e7afce0128b0
DavidGarzonDev <juanxavif@gmail.com>
2025-07-05 15:56:56 -0500

    añadiendo los componentes en las rutas

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,19 +1,25 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import { Navbar } from "../components/shared/Navbar"
 import { Footer } from "../components/shared/Footer"
+import { Banner } from "../components/home/Banner"
+import { Newsletter } from "../components/home/Newsletter"
 
 
 export const RootLayout = () => {
+    const { pathname } = useLocation()
     return (
         <div className="h-screen flex flex-col bg-gray-100">
             <Navbar />
-            <main className="container my-8 flex-1">
-                <Outlet />
-                
-            </main>
+                {pathname === '/' && <Banner/>}
             
+                <main className="container my-8 flex-1 mx-auto">
+                    <Outlet />
+                </main>
+                {pathname === '/' && <Newsletter/>}
 
-            <Footer/>
+
+
+            <Footer />
         </div>
     )
 }
