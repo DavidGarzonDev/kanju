1e7efd8433c71e053e15cfdceda5dd48bae9f07d
DavidGarzonDev <juanxavif@gmail.com>
2025-07-04 20:25:43 -0500

    añadiendo las constantes de los links

--- src/constants/Links.tsx
+++ src/constants/Links.tsx
@@ -1,3 +1,5 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6"
+
 export const navbarLinks = [
     {   id: 1,
         title: "Inicio",
@@ -12,5 +14,26 @@ export const navbarLinks = [
         id: 3,
         title: "Mujer",
         href: "/woman"
+    }
+]
+
+export const socialLinks = [
+    {
+        id: 1,
+        title: "Facebook",
+        href: "https://www.facebook.com",
+        icon: <FaFacebookF/>
+    },
+    {
+        id: 2,
+        title: "Instagram",
+        href: "https://www.instagram.com",
+        icon: <FaInstagram/>
+    },
+    {
+        id: 3,
+        title: "Twitter",
+        href: "https://www.twitter.com",
+        icon: <FaTwitter/>
     }
 ]
