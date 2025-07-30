780bdd1cd10d9f14fe22f554842b0352196e7ec2
DavidGarzonDev <juanxavif@gmail.com>
2025-07-30 18:03:54 -0500

    mejorando el responsive en el card

--- src/components/home/ProductGrid.tsx
+++ src/components/home/ProductGrid.tsx
@@ -12,7 +12,7 @@ const ProductGrid = ({ title, products }: Props) => {
                 {title}
             </h2>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-0">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-0">
                 {products.map((product, index) => (
                     <CardProduct
                         key={product.id || `${product.slug}-${index}`}
